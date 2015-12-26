16558cf6165eac6a41796a4aeed8e417658f4ff6
Dustin D. Clark <dustin@captivatelabs.com>
2015-12-26 12:35:54 -0600

    Prevent null pointer.

--- src/groovy/com/captivatelabs/grails/timezone/detection/TimeZoneUtil.groovy
+++ src/groovy/com/captivatelabs/grails/timezone/detection/TimeZoneUtil.groovy
@@ -3,6 +3,9 @@ package com.captivatelabs.grails.timezone.detection
 final class TimeZoneUtil {
 
     static Date offsetDate(TimeZone fromTimezone, TimeZone toTimezone, Date dateToOffset) {
+        if (!fromTimezone || !toTimezone) {
+            return dateToOffset
+        }
         long fromOffset = fromTimezone.getRawOffset() + (fromTimezone.inDaylightTime(dateToOffset) ? fromTimezone.getDSTSavings() : 0)
         long toOffset = toTimezone.getRawOffset() + (toTimezone.inDaylightTime(dateToOffset) ? toTimezone.getDSTSavings() : 0)
         long adjustment = toOffset - fromOffset
